c58efb4ab1766936a9638fc5da978bd3314a922a
sintakt <neu_stuempfe0e@icloud.com>
2022-02-28 14:49:50 +0100

    update pages branch

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<soundpool/SoundpoolPlugin.h>)
+#import <soundpool/SoundpoolPlugin.h>
+#else
+@import soundpool;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SoundpoolPlugin registerWithRegistrar:[registry registrarForPlugin:@"SoundpoolPlugin"]];
+}
+
+@end
